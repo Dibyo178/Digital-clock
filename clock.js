2893bcc7894d1672d4b3acc53fd7c6b639d1e655
Dibyo178 <Sourov.mu@gmail.com>
2022-04-06 08:52:24 +0600

    done

---
+++ clock.js
@@ -0,0 +1,56 @@
+
+let hr =document.querySelector("#hr");
+let mn = document.querySelector("#mn");
+let sc =document.querySelector("#sc");
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * 6;
+    let ss = day.getSeconds() * 6;
+    
+    hr.style.transform= `rotate(${hh+(mm/12)}deg)`;
+    mn.style.transform= `rotate(${mm}deg)`;
+    sc.style.transform= `rotate(${ss}deg)`;
+
+
+    //  digital clock
+
+let hours = document.getElementById('hours');
+let miniutes = document.getElementById('miniutes');
+let seconds = document.getElementById('seconds');
+
+let amp = document.getElementById('ampm');
+
+
+let h = new Date().getHours();
+let m = new Date().getMinutes();
+let s = new Date().getSeconds();
+
+let am = h >= 12 ? "PM" : "AM";
+
+//  convert 24 hour
+if(h >12){
+    h = h - 12;
+}
+
+
+//  add zero
+
+h = (h<10)?"0" + h : h;
+m = (m<10)?"0" + m : m;
+s = (s<10)?"0" + s : s;
+
+hours.innerHTML = h;
+miniutes.innerHTML = m;
+seconds.innerHTML = s;
+amp.innerHTML=am;
+
+
+
+
+
+
+
+})
+
